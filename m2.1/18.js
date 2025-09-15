6f09117f7039559adc33f5f1eda97c2319bfdabf
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2025-09-15 18:58:38 +0000

    Terminar ainda hj

---
+++ m2.1/18.js
@@ -0,0 +1,26 @@
+let val1 = 0
+let val2 = 1
+let val3 = ""
+let val4 = "Hello"
+let val5 = null
+let val6 = undefined
+let val7 = {}
+let val8 = []
+
+let boo1 = Boolean(val1)
+let boo2 = Boolean(val2)
+let boo3 = Boolean(val3)
+let boo4 = Boolean(val4)
+let boo5 = Boolean(val5)
+let boo6 = Boolean(val6)
+let boo7 = Boolean(val7)
+let boo8 = Boolean(val8)
+
+console.log(boo1)
+console.log(boo2)
+console.log(boo3)
+console.log(boo4)
+console.log(boo5)
+console.log(boo6)
+console.log(boo7)
+console.log(boo8)
